3ff9202534550cd595de56c068857f3792004424
Ítalo <italo.ts4895@gmail.com>
2025-06-08 00:01:11 -0300

    Added clock view for in game time

---
+++ Assets/Scripts/UI/ClockView.cs
@@ -0,0 +1,30 @@
+using System;
+using TMPro;
+using Toblerone.Toolbox;
+using UnityEngine;
+
+namespace FarmValley {
+    public class ClockView : MonoBehaviour {
+        [SerializeField] private FloatVariable timeVariable;
+        [SerializeField] private TextMeshProUGUI textField;
+        private VariableObserver<float> timeObserver;
+
+        private void Awake() {
+            timeObserver = new VariableObserver<float>(timeVariable, UpdateTimer);
+        }
+
+        private void UpdateTimer(float newTimeSeconds) {
+            int totalSeconds = Mathf.FloorToInt(Mathf.Max(0f, newTimeSeconds));
+            int minutes = Math.DivRem(totalSeconds, 60, out int seconds);
+            textField.text = $"{minutes:00}:{seconds:00}";
+        }
+
+        private void OnEnable() {
+            timeObserver.StartWatching();
+        }
+
+        private void OnDisable() {
+            timeObserver.StopWatching();
+        }
+    }
+}
